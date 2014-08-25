9cfbad0e4b8dde82166b14637af3cc501f40b212
appdev2014 <home@Homes-MacBook-Pro.local>
2014-08-25 21:27:08 +0100

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Home on 25/08/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Home on 25/08/2014.
+//
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
